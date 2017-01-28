f6f889ff362914110b1a507b2970052c11e88b13
FerencOnodi <onodiferenc91@gmail.com>
2017-01-28 16:09:53 +0100

    Initialised Yes.java

---
+++ Wednesday/Yes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Yes {
+
+	public static void main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter a word please! ");
+		String next = reader.next();
+	}
+}

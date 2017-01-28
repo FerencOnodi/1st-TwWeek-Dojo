f11d3ed7d79dd5606938b8484e35ea5ed399034b
FerencOnodi <onodiferenc91@gmail.com>
2017-01-28 16:30:06 +0100

    Updated Yes.java

--- Wednesday/Yes.java
+++ Wednesday/Yes.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
 public class Yes {
+	
+	//WORKS WITH COMMAND LINE ARGUMENT
+	public static void main(String[] args) {
+		String userInput = args[0].toLowerCase();
+		
+		if (userInput.equals("yes")) {
+			System.out.println("Ok. You have entered the right word.");
+		} else {
+			System.out.println("Sorry this is not the right word...");
+		}
+	}
 
+
+	//WORKS WITH USER INPUT FROM TERMINAL
+	/*
 	public static void main(String[] args) {
 
 		Scanner userInput = new Scanner(System.in);
 		System.out.print("Enter a word please! ");
-		String next = reader.next();
-	}
+		String userIn = userInput.next();
+		
+		if(userIn.toLowerCase().equals("yes")) {
+			System.out.println("Ok. You have entered the right word.");
+		} else {
+			System.out.println("Sorry this is not the right word...");
+		}
+	}*/
 }
